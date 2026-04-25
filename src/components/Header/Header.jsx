111ed54f21f73df4d97119dc4249bc3573963c0c
cassius269 <fahamygaston@gmail.com>
2026-04-26 00:43:25 +0200

    stylisation de lien actif

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
-import { Link } from "react-router";
+import { NavLink, useMatch } from "react-router";
 import styles from "../../assets/styles/layouts/Header.module.scss";
+
 function Header() {
+  const isActive = useMatch("/profile");
+
+  console.log(isActive);
   return (
     <header
       className={`container-fluid border border-2 p-2 d-flex justify-content-between ${styles.header}`}
     >
       <nav className="d-flex justify-content-around w-100">
-        <Link to="/">
+        <NavLink to="/">
           <img
             src="https://cdn-icons.flaticon.com/svg/7434/7434561.svg?token=exp=1777122008~hmac=aa0d334b625f8c56eb446e8911fce110"
             alt="logo"
             width={30}
           />
-        </Link>
+        </NavLink>
 
         <ul className="d-flex flex-columns gap-3">
-          <Link to="/">Homepage</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/fdfds">eFzfzef</Link>
+          <NavLink to="/">Homepage</NavLink>
+          <NavLink to="/profile">Profile{isActive && <i>+</i>}</NavLink>
+          <NavLink to="/fdfds">eFzfzef</NavLink>
         </ul>
       </nav>
     </header>
