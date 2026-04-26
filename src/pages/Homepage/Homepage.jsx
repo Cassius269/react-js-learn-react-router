45cc5af570318bee97db3651393340a38ac6eab6
cassius269 <fahamygaston@gmail.com>
2026-04-26 21:59:32 +0200

    récupération des recetes à la page d'accueil

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 function Homepage() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      const response = await fetch("https://www.restapi.fr/api/recipes");
+
+      if (response.ok) {
+        const data = await response.json();
+        setRecipes(Array.isArray(data) ? data : [data]);
+        console.log(data);
+      } else {
+        console.log("Ooops une erreur");
+      }
+    };
+
+    getRecipes();
+  }, []);
   return (
     <>
       <h1>Je suis la page d'accueil</h1>
+      {recipes.length > 0 && (
+        <ul>
+          {recipes.map((r, index) => (
+            <li
+              key={r._id}
+              className={`${index % 2 === 0 ? "text-secondary" : ""}`}
+            >
+              {r.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
