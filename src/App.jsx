8054710434fa40575c3ba3d69a634a07b8aab624
cassius269 <fahamygaston@gmail.com>
2026-04-27 00:39:22 +0200

    création et affichage d'un loader globale à la racine de l'App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Outlet, ScrollRestoration } from "react-router";
+import { Outlet, ScrollRestoration, useNavigation } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+
+function Loading() {}
 
 function App() {
   // const location = useLocation();
   // console.log(location);
 
+  const { state } = useNavigation();
+  console.log(state);
+
   return (
     <>
       <Header />
       <main>
+        {/** Le loader global de l'app */}
+        {state === "loading" && (
+          <h2 className="mt-5">
+            Chargement en cours...depuis le loader globale
+          </h2>
+        )}
         {/** Outlet permet de remplacer le contenu du composant en fonction de l'URL saisi par l'utilisateur */}
         <Outlet />
       </main>
--- src/loaders/hompageLoader.jsx
+++ src/loaders/hompageLoader.jsx
@@ -1,5 +1,7 @@
 export async function homepageLoader() {
-  const response = await fetch("https://www.restapi.fr/api/recipes");
+  const response = await fetch("https://www.restapi.fr/api/recipes?delay=3");
+
+  //   throw new Error("Oops, une erreur");
 
   if (response.ok) {
     return {
