ed2ff3f7231d7f4bbd24277dff492b5b26ccba39
cassius269 <fahamygaston@gmail.com>
2026-04-29 16:14:24 +0200

    suppression du composant dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import { ROUTER } from "./components/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={ROUTER}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={ROUTER} />
   </StrictMode>,
 );
